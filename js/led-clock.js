0d4c52b83d3f88e172894680ac3c1e4a58e43cd8
Darren Embry <dse@webonastick.com>
2020-11-03 10:48:03 -0500

    fixes

--- js/led-clock.js
+++ js/led-clock.js
@@ -1,3 +1,119 @@
+/*global Symbol, AudioContext, console */
+
+/**
+ * requestAnimationFrame polyfill
+ * https://gist.github.com/paulirish/1579671
+ */
+(function () {
+    var lastTime = 0;
+    var vendors = ['ms', 'moz', 'webkit', 'o'];
+    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
+        window.cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame'] || window[vendors[x] + 'CancelRequestAnimationFrame'];
+    }
+    if (!window.requestAnimationFrame) {
+        window.requestAnimationFrame = function (callback, element) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var id = window.setTimeout(
+                function () { callback(currTime + timeToCall); },
+                timeToCall
+            );
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+    }
+    if (!window.cancelAnimationFrame) {
+        window.cancelAnimationFrame = function (id) {
+            clearTimeout(id);
+        };
+    }
+}());
+
+/**
+ * Array.from polyfill
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/from
+ */
+if (!Array.from) {
+    Array.from = (function () {
+        var symbolIterator;
+        try {
+            symbolIterator = Symbol.iterator ? Symbol.iterator : 'Symbol(Symbol.iterator)';
+        } catch (e) {
+            symbolIterator = 'Symbol(Symbol.iterator)';
+        }
+        var toStr = Object.prototype.toString;
+        var isCallable = function (fn) {
+            return typeof fn === 'function' || toStr.call(fn) === '[object Function]';
+        };
+        var toInteger = function (value) {
+            var number = Number(value);
+            if (isNaN(number)) {
+                return 0;
+            }
+            if (number === 0 || !isFinite(number)) {
+                return number;
+            }
+            return (number > 0 ? 1 : -1) * Math.floor(Math.abs(number));
+        };
+        var maxSafeInteger = Math.pow(2, 53) - 1;
+        var toLength = function (value) {
+            var len = toInteger(value);
+            return Math.min(Math.max(len, 0), maxSafeInteger);
+        };
+        var setGetItemHandler = function setGetItemHandler(isIterator, items) {
+            var iterator = isIterator && items[symbolIterator]();
+            return function getItem(k) {
+                return isIterator ? iterator.next() : items[k];
+            };
+        };
+        var getArray = function getArray(T, A, len, getItem, isIterator, mapFn) {
+            var k = 0;
+            while (k < len || isIterator) {
+                var item = getItem(k);
+                var kValue = isIterator ? item.value : item;
+                if (isIterator && item.done) {
+                    return A;
+                } else {
+                    if (mapFn) {
+                        A[k] = typeof T === 'undefined' ? mapFn(kValue, k) : mapFn.call(T, kValue, k);
+                    } else {
+                        A[k] = kValue;
+                    }
+                }
+                k += 1;
+            }
+            if (isIterator) {
+                throw new TypeError('Array.from: provided arrayLike or iterator has length more then 2 ** 52 - 1');
+            } else {
+                A.length = len;
+            }
+            return A;
+        };
+        return function from(arrayLikeOrIterator /*, mapFn, thisArg */) {
+            var C = this;
+            var items = Object(arrayLikeOrIterator);
+            var isIterator = isCallable(items[symbolIterator]);
+            if ((arrayLikeOrIterator === null || arrayLikeOrIterator === undefined) && !isIterator) {
+                throw new TypeError('Array.from requires an array-like object or iterator - not null or undefined');
+            }
+            var mapFn = arguments.length > 1 ? arguments[1] : void undefined;
+            var T;
+            if (typeof mapFn !== 'undefined') {
+                if (!isCallable(mapFn)) {
+                    throw new TypeError('Array.from: when provided, the second argument must be a function');
+                }
+                if (arguments.length > 2) {
+                    T = arguments[2];
+                }
+            }
+            var len = toLength(items.length);
+            var A = isCallable(C) ? Object(new C(len)) : new Array(len);
+            return getArray(T, A, len, setGetItemHandler(isIterator, items), isIterator, mapFn);
+        };
+    })();
+}
+
 var TimeTicker = (function () {
     function TimeTicker(options) {
     }
@@ -73,43 +189,6 @@ if (window.AudioContext && (location.protocol === 'http:' || location.protocol =
         return LEDClockAudio;
     }());
 }
-
-/**
- * requestAnimationFrame polyfill
- */
-
-(function () {
-    var raf = window.requestAnimationFrame;
-    raf = raf || window.mozRequestAnimationFrame;
-    raf = raf || window.webkitRequestAnimationFrame;
-    raf = raf || window.msRequestAnimationFrame;
-    raf = raf || window.oRequestAnimationFrame;
-    window.requestAnimationFrame = raf;
-
-    var caf = window.cancelAnimationFrame;
-    caf = caf || window.mozCancelAnimationFrame;
-    caf = caf || window.webkitCancelAnimationFrame;
-    caf = caf || window.msCancelAnimationFrame;
-    caf = caf || window.oCancelAnimationFrame;
-    caf = caf || window.mozCancelRequestAnimationFrame;
-    caf = caf || window.webkitCancelRequestAnimationFrame;
-    caf = caf || window.msCancelRequestAnimationFrame;
-    caf = caf || window.oCancelRequestAnimationFrame;
-    window.cancelAnimationFrame = caf;
-
-    if (!window.requestAnimationFrame || !window.cancelAnimationFrame) {
-        window.requestAnimationFrame = function (callback, element) {
-            var currTime = new Date().getTime();
-            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-            var id = window.setTimeout(function () { callback(currTime + timeToCall); }, timeToCall);
-            lastTime = currTime + timeToCall;
-            return id;
-        };
-        window.cancelAnimationFrame = function (id) {
-            clearTimeout(id);
-        };
-    }
-}());
 
 // stackoverflow says absolute URLs work in safari
 var tickURL = absoluteURL('sounds/tick2.wav');
